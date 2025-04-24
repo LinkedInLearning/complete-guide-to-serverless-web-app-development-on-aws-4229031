6a522f570e7a34a2ba0a805da5a8cb4a41203e3e
mavi888 <7993283+mavi888@users.noreply.github.com>
2025-04-24 14:57:31 +0300

    Creating the todo-app-infra and adding amplify hosting resource

---
+++ todo-app-infra/bin/todo-app-infra.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { AmplifyHostingStack } from '../lib/amplify-stack';
+
+const app = new cdk.App();
+
+const amplifyStack = new AmplifyHostingStack(app, 'TodoAppAmplifyHostingStack', {});
---
+++ todo-app-infra/lib/amplify-stack.ts
@@ -0,0 +1,50 @@
+import { CfnOutput, SecretValue, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+	App,
+	GitHubSourceCodeProvider,
+	RedirectStatus,
+} from '@aws-cdk/aws-amplify-alpha';
+
+interface AmplifyStackProps extends StackProps {
+}
+
+
+export class AmplifyHostingStack extends Stack {
+	constructor(scope: Construct, id: string, props: AmplifyStackProps) {
+		super(scope, id, props);
+
+        // Create the Amplify application
+		const amplifyApp = new App(this, `TodoWebApp`, {
+			sourceCodeProvider: new GitHubSourceCodeProvider({
+				owner: 'mavi888',
+				repository: 'linkedin-todo-webapp',
+				oauthToken: SecretValue.secretsManager('github-token'),
+			}),
+			environmentVariables: {
+				REGION: this.region,
+            },
+		});
+
+        // Add a branch
+        const main = amplifyApp.addBranch('main', {
+            autoBuild: true,
+            stage: 'PRODUCTION'
+        });
+
+        amplifyApp.addCustomRule({
+			source:
+				'</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|woff2|ttf|map|json|webp)$)([^.]+$)/>',
+			target: '/index.html',
+			status: RedirectStatus.REWRITE,
+		});
+
+        new CfnOutput(this, 'AmplifyAppName', {
+			value: amplifyApp.appName,
+		});
+
+		new CfnOutput(this, 'AmplifyURL', {
+			value: `https://main.${amplifyApp.defaultDomain}`,
+		});
+    }
+}
