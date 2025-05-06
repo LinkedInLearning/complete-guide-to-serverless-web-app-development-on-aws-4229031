3dd5396513ed52efc32823a593890d057b141338
mavi888 <7993283+mavi888@users.noreply.github.com>
2025-05-06 17:44:17 +0300

    Using real user in lambda function

--- todo-app-infra/lambda/index.ts
+++ todo-app-infra/lambda/index.ts
@@ -24,10 +24,9 @@ export const handler = async (event: any = {}): Promise<any> => {
 
         console.log(httpMethod);
         console.log(path);
-
-         //FAKE USER ID
-         const userId = 'MR_FAKE';
-
+  
+        const userId = event.requestContext.authorizer?.claims['cognito:username'];
+      
          switch (httpMethod) {
             case 'GET':
                 console.log('GET all TODOs');
